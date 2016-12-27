f0380385d2921309e761f143be6843e1ccaf6a0c
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2016-12-26 20:42:50 -0500

    Use a for loop to create and display TextViews of numbers

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,7 +2,8 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -16,6 +17,7 @@ public class NumbersActivity extends AppCompatActivity
 
         //Create an ArrayList of words
         ArrayList<String> words = new ArrayList<>();
+
         words.add("one");
         words.add("two");
         words.add("three");
@@ -27,6 +29,18 @@ public class NumbersActivity extends AppCompatActivity
         words.add("nine");
         words.add("ten");
 
-        Log.v("NumbersActivity", words.toString());
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+        for (int i = 0; i < words.size(); i++)
+        {
+            // Create a new TextView
+            TextView wordView = new TextView(this);
+
+            // Set the text to be word at the current index
+            wordView.setText(words.get(i));
+
+            // Add this TextView as another child to the rootView of this layout
+            rootView.addView(wordView);
+        }
     }
 }
