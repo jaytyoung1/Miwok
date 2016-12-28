55f9073f0e16ef5a93c04c604c706af210bb9b15
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2016-12-28 11:17:11 -0500

    Add method to determine if word has an associated image

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -14,7 +14,9 @@ public class Word
     private String mMiwokTranslation;
 
     // Image resource ID for the word
-    private int mImageResourceId;
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+
+    private static final int NO_IMAGE_PROVIDED = -1;
 
     /**
      * Create a Word object
@@ -64,5 +66,13 @@ public class Word
     public int getImageResourceId()
     {
         return mImageResourceId;
+    }
+
+    /**
+     * Returns whether or not there is an image for this word
+     */
+    public boolean hasImage()
+    {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
     }
 }
